2d6b89366095a4b7d5736d91d60beda494665c30
Munsi Abdul Al Mamun <mdabdulalmamoon@gmail.com>
2022-01-20 14:36:12 +0800

    Add project files.

---
+++ ArrayApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            
+            numbers[2] = 3;
+            numbers[3] = 4;
+
+            Console.WriteLine(numbers[2]);
+            Console.ReadLine();
+        }
+    }
+}
